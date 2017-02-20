9ebae217ded27ece42acb86c407572c5605ff8e4
Sidnev <alexey.sidnev@intel.com>
2017-02-20 13:18:12 +0300

    Work is done

---
+++ include/tree.h
@@ -0,0 +1,24 @@
+#ifndef INCLUDE_TREE_H_
+#define INCLUDE_TREE_H_
+
+#include <stdio.h>
+#include <queue>
+#include <time.h>
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+struct Branch {
+    int data;
+    Branch *left;
+    Branch *right;
+};
+
+void Init_tree(Branch **tree);
+Branch *Add_leaf(Branch *root, int val);
+void Print_tree(Branch *tree);
+Branch **find(Branch **root, int value);
+
+#endif  // INCLUDE_TREE_H_
