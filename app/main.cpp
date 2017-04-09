468864b064a493605b2ee382b714b727e5365863
Maxim Ermolaev <makcoh4nk@mail.ru>
2017-04-09 21:54:20 +0300

    version 1.0

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,5 @@
-#include "Way.h"
-#include <iostream>
+#include "Dijkstra.h"
 
 int main() {
-  std::cout << "2 + 2 = " << add(2, 2) << std::endl;
+    return 0;
 }
---
+++ src/Dijkstra.cpp
@@ -0,0 +1,26 @@
+#include <set>
+#include "Dijkstra.h"
+
+std::vector<int> Dijkstra(std::vector <std::vector <std::pair <int, int > > > v, int start) {
+    if (v.size() == 0) {
+        throw std::logic_error("Graph size = 0");
+    }
+    std::vector<int> dist(v.size(), INF);
+    dist[start] = 0;
+    std::set< std::pair<int, int> > unused;
+    unused.insert(std::make_pair(0, start));
+    while (!unused.empty()) {
+        int i = unused.begin()->second;
+        unused.erase(unused.begin());
+        for (auto edge : v[i]) {
+            int j = edge.first;
+            int wt = edge.second;
+            if (dist[i] + wt < dist[j]) {
+                unused.erase(std::make_pair(dist[j], j));
+                dist[j] = dist[i] + wt;
+                unused.insert(std::make_pair(dist[j], j));
+            }
+        }
+    }
+    return dist;
+}
