a212008f4148ed0bd1a318ac2787356947827eb1
Maxim Ermolaev <makcoh4nk@mail.ru>
2017-06-02 21:19:10 +0300

    fix tests

--- test/test_Dijkstra.cpp
+++ test/test_Dijkstra.cpp
@@ -7,20 +7,20 @@ using std::make_pair;
 vector< vector< std::pair< int, int > > > InitTestGraph() {
     vector< vector< std::pair< int, int > > > test(7);
     auto it = test.begin();
-    it->push_back(make_pair(4, 1));
-    it->push_back(make_pair(6, 2));
+    it->push_back(make_pair(1, 4));
+    it->push_back(make_pair(2, 6));
     it++;
-    it->push_back(make_pair(1, 3));
+    it->push_back(make_pair(3, 1));
     it++;
     it->push_back(make_pair(3, 3));
-    it->push_back(make_pair(2, 4));
+    it->push_back(make_pair(4, 2));
     it++;
-    it->push_back(make_pair(1, 5));
+    it->push_back(make_pair(5, 1));
     it++;
-    it->push_back(make_pair(4, 6));
-    it->push_back(make_pair(1, 5));
+    it->push_back(make_pair(6, 1));
+    it->push_back(make_pair(5, 1));
     it++;
-    it->push_back(make_pair(2, 6));
+    it->push_back(make_pair(6, 2));
     return test;
 }
 
